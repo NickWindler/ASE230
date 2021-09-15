aeb8c05676b9874432a680f2e53d3343fead6129
NickWindler <89546808+NickWindler@users.noreply.github.com>
2021-09-15 16:10:52 -0400

    Add files via upload

--- Assignment2/detail.php
+++ Assignment2/detail.php
@@ -1,5 +1,73 @@
 <?php
-	$students = array("Nick Windler" => 4, "John Doe" => 2, "Person One" => 3)
+$students = [
+	[
+		'name' => "Nick Windler",
+		'grade' => 4,
+		'job' => "Live Streaming",
+		'company' => "Twitch",
+		'email' => "windlern1@nku.edu",
+		'intro' => "While born in Indiana, I have spent the majority of my life living in LaGrange Kentucky.
+					I went to Oldham County Highschool and graduated in 2018. I made the decision to go to 
+					college here at NKU and now I am currently a senior CIT major getting ready to graduate 
+					this coming spring. I am currently working at the Cincinnati Insurance Company as an IT 
+					Resource Management Intern where I help develop apps on a small team. I expect I will 
+					continue doing some type of dev work in the future as well.",
+		'quote' => '"I can not think of a quote to put here" -Nick Windler',
+		'skills' => [
+			"Cooking",
+			"Information Technologies",
+			"Random Knowledge That Does Not Help Me in Life",
+		],
+		'percentages' => [
+			"1%",
+			"50%",
+			"90%"
+		],
+		'fact' => "I have two cats with three legs, I have been to the Grand Canyon, and I can eat a lot of 
+					Spaghetti in one sitting."
+	],
+	[
+		'name' => "John Doe",
+		'grade' => 2,
+		'job' => "Accountant",
+		'company' => "General Electric",
+		'email' => "doejohn@gmail.com",
+		'intro' => "I want to become an accountant to commit numerous amounts of white collar crime like Accounting Fraud.",
+		'quote' => '"You Either Die A Hero, Or You Live Long Enough To See Yourself Become The Villain" -Harvey Dent',
+		'skills' => [
+			"Being a Respectable Citizen",
+			"Accounting",
+			"Committing Crimes"
+		],
+		'percentages' => [
+			"5%",
+			"50%",
+			"99%"
+		],
+		'fact' => "I have never been to jail and I enjoy watching the news."
+	],
+	[
+		'name' => "Person One",
+		'grade' => 3,
+		'job' => "Chef",
+		'company' => "Create My Own",
+		'email' => "Personchef@gmail.com",
+		'intro' => "Hello, my name is person one. I was named Person One because the person who 
+					created me is not very creative so they could not think of anything else.",
+		'quote' => '"It really be like that sometimes" -anonymous',
+		'skills' => [
+			"Being a Fun Person",
+			"Manning the Grill at a Cookout",
+			"Cooking"
+		],
+		'percentages' => [
+			"20%",
+			"70%",
+			"95%"
+		],
+		'fact' => "I am not a very fun person"
+	]
+];
 ?>
 <!doctype html>
 <html lang="en">
@@ -14,7 +82,7 @@
 	<body>
 		<?php if($_GET['name'] == 'Nick Windler'):?>
 			<div class="container text-center mb-5">
-				<h1><?="This is ASE 230 - Nick Windler"?></h1>
+				<h1><?='This is ASE 230 -' . $_GET['name']?></h1>
 			</div>
 			<div class="container">
 				<div class="row">
@@ -23,7 +91,7 @@
 						  <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="">
 						</div>
 						<div class="mb-2 d-flex">
-							<h4 class="font-weight-normal"><?='Nick Windler'?></h4>
+							<h4 class="font-weight-normal"><?=$_GET['name']?></h4>
 							<div class="social d-flex ml-auto">
 								<p class="pr-2 font-weight-normal">Follow on:</p>
 								<a href="#" class="text-muted mr-1">
@@ -44,24 +112,24 @@
 							<ul class="list-unstyled">
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Dream profession:'?></span>
-									<label class="media-body"><?='Live Streaming '?></label>
+									<label class="media-body"><?=$students[0]['job']?></label>
 								</li>
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Dream company:'?></span>
-									<label class="media-body"><?='Twitch'?></label>
+									<label class="media-body"><?=$students[0]['company']?></label>
 								</li>
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Email: '?></span>
-									<label class="media-body"><?='Windlern1@nku.edu'?></label>
+									<label class="media-body"><?=$students[0]['email']?></label>
 								</li>
 							</ul>
 						</div>
 					</div>
 					<div class="col-lg-7 col-md-6 pl-xl-3">
 						<h5 class="font-weight-normal">Short intro</h5>
-						<p><?='While born in Indiana, I have spent the majority of my life living in LaGrange Kentucky. I went to Oldham County Highschool and graduated in 2018. I made the decision to go to college here at NKU and now I am currently a senior CIT major getting ready to graduate this coming spring. I am currently working at the Cincinnati Insurance Company as an IT Resource Management Intern where I help develop apps on a small team. I expect I will continue doing some type of dev work in the future as well. '?></p>
+						<p><?=$students[0]['intro']?></p>
 						<div class="my-2 bg-light p-2">
-							<p class="font-italic mb-0"><?='"I can not think of a quote to put here" -Nick Windler'?></p>
+							<p class="font-italic mb-0"><?=$students[0]['quote']?></p>
 						</div>
 						<div class="mb-2 mt-2 pt-1">
 							<h5 class="font-weight-normal">Top skills</h5>
@@ -69,29 +137,29 @@
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar-bad" role="progressbar" style="width:1%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Cooking'?></div>
-									<span class="progress-bar-number"><?='1%'?></span>
+									<div class="progress-bar-title"><?= $students[0]['skills'][0]?></div>
+									<span class="progress-bar-number"><?= $students[0]['percentages'][0]?></span>
 								</div>
 							</div>
 						</div>
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar" role="progressbar" style="width:50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Information Technologies'?></div>
-									<span class="progress-bar-number"><?='50%'?></span>			  
+									<div class="progress-bar-title"><?= $students[0]['skills'][1]?></div>
+									<span class="progress-bar-number"><?= $students[0]['percentages'][1]?></span>			  
 								</div>
 							</div>
 						</div>
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar" role="progressbar" style="width:90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Random Knowledge That Does Not Help Me in Life'?></div>
-									<span class="progress-bar-number"><?='90%'?></span>
+									<div class="progress-bar-title"><?= $students[0]['skills'][2]?></div>
+									<span class="progress-bar-number"><?= $students[0]['percentages'][2]?></span>
 								</div>
 							</div>
 						</div>
 						<h5 class="font-weight-normal">Fun fact</h5>
-						<p><?='I have two cats with three legs, I have been to the Grand Canyon, and I can eat a lot of Spaghetti in one sitting. '?></p>
+						<p><?= $students[0]['fact']?></p>
 						<a href="index.php"><?='Back To Index'?></a>
 					</div>
 				</div>
@@ -101,7 +169,7 @@
 		<?php endif;?>
 		<?php if($_GET['name'] == 'John Doe'):?>
 			<div class="container text-center mb-5">
-				<h1><?="This is ASE 230 - John Doe"?></h1>
+				<h1><?='This is ASE 230 -' . $_GET['name']?></h1>
 			</div>
 			<div class="container">
 				<div class="row">
@@ -110,7 +178,7 @@
 						  <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
 						</div>
 						<div class="mb-2 d-flex">
-							<h4 class="font-weight-normal"><?='John Doe'?></h4>
+							<h4 class="font-weight-normal"><?=$_GET['name']?></h4>
 							<div class="social d-flex ml-auto">
 								<p class="pr-2 font-weight-normal">Follow on:</p>
 								<a href="#" class="text-muted mr-1">
@@ -131,24 +199,24 @@
 							<ul class="list-unstyled">
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Dream profession:'?></span>
-									<label class="media-body"><?='Accountant'?></label>
+									<label class="media-body"><?=$students[1]['job']?></label>
 								</li>
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Dream company:'?></span>
-									<label class="media-body"><?='General Electric'?></label>
+									<label class="media-body"><?=$students[1]['company']?></label>
 								</li>
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Email: '?></span>
-									<label class="media-body"><?='doejohn@gmail.com'?></label>
+									<label class="media-body"><?=$students[1]['email']?></label>
 								</li>
 							</ul>
 						</div>
 					</div>
 					<div class="col-lg-7 col-md-6 pl-xl-3">
 						<h5 class="font-weight-normal">Short intro</h5>
-						<p><?='I want to become an accountant to commit numerous amounts of white collar crime like Accounting Fraud.'?></p>
+						<p><?=$students[1]['intro']?></p>
 						<div class="my-2 bg-light p-2">
-							<p class="font-italic mb-0"><?='"You Either Die A Hero, Or You Live Long Enough To See Yourself Become The Villain" -Harvey Dent'?></p>
+							<p class="font-italic mb-0"><?=$students[1]['quote']?></p>
 						</div>
 						<div class="mb-2 mt-2 pt-1">
 							<h5 class="font-weight-normal">Top skills</h5>
@@ -156,29 +224,29 @@
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar-bad" role="progressbar" style="width:5%" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Being a Respectable Citizen'?></div>
-									<span class="progress-bar-number"><?='5%'?></span>
+									<div class="progress-bar-title"><?= $students[1]['skills'][0]?></div>
+									<span class="progress-bar-number"><?= $students[1]['percentages'][0]?></span>
 								</div>
 							</div>
 						</div>
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar" role="progressbar" style="width:50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Accounting'?></div>
-									<span class="progress-bar-number"><?='50%'?></span>			  
+									<div class="progress-bar-title"><?= $students[1]['skills'][1]?></div>
+									<span class="progress-bar-number"><?= $students[1]['percentages'][1]?></span>			  
 								</div>
 							</div>
 						</div>
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar" role="progressbar" style="width:99%" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Committing Crimes'?></div>
-									<span class="progress-bar-number"><?='99%'?></span>
+									<div class="progress-bar-title"><?= $students[1]['skills'][2]?></div>
+									<span class="progress-bar-number"><?= $students[1]['percentages'][2]?></span>
 								</div>
 							</div>
 						</div>
 						<h5 class="font-weight-normal">Fun fact</h5>
-						<p><?='I have never been to jail and I enjoy watching the news.'?></p>
+						<p><?= $students[1]['fact']?></p>
 						<a href="index.php"><?='Back To Index'?></a>
 					</div>
 				</div>
@@ -188,7 +256,7 @@
 		<?php endif;?>
 		<?php if($_GET['name'] == 'Person One'):?>
 			<div class="container text-center mb-5">
-				<h1><?="This is ASE 230 - Person One"?></h1>
+				<h1><?='This is ASE 230 -' . $_GET['name']?></h1>
 			</div>
 			<div class="container">
 				<div class="row">
@@ -197,7 +265,7 @@
 						  <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="">
 						</div>
 						<div class="mb-2 d-flex">
-							<h4 class="font-weight-normal"><?='Person One'?></h4>
+							<h4 class="font-weight-normal"><?=$_GET['name']?></h4>
 							<div class="social d-flex ml-auto">
 								<p class="pr-2 font-weight-normal">Follow on:</p>
 								<a href="#" class="text-muted mr-1">
@@ -218,24 +286,24 @@
 							<ul class="list-unstyled">
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Dream profession:'?></span>
-									<label class="media-body"><?='Chef'?></label>
+									<label class="media-body"><?=$students[2]['job']?></label>
 								</li>
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Dream company:'?></span>
-									<label class="media-body"><?='Create my Own'?></label>
+									<label class="media-body"><?=$students[2]['company']?></label>
 								</li>
 								<li class="media">
 									<span class="w-25 text-black font-weight-normal"><?='Email: '?></span>
-									<label class="media-body"><?='personchef@gmail.com'?></label>
+									<label class="media-body"><?=$students[2]['email']?></label>
 								</li>
 							</ul>
 						</div>
 					</div>
 					<div class="col-lg-7 col-md-6 pl-xl-3">
 						<h5 class="font-weight-normal">Short intro</h5>
-						<p><?='Hello, my name is person one. I was named Person One because the person who created me is not very creative so they could not think of anything else.'?></p>
+						<p><?=$students[1]['intro']?></p>
 						<div class="my-2 bg-light p-2">
-							<p class="font-italic mb-0"><?='"It really be like that sometimes" -anonymous'?></p>
+							<p class="font-italic mb-0"><?=$students[2]['quote']?></p>
 						</div>
 						<div class="mb-2 mt-2 pt-1">
 							<h5 class="font-weight-normal">Top skills</h5>
@@ -243,29 +311,29 @@
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar-bad" role="progressbar" style="width:20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Being a Fun Person'?></div>
-									<span class="progress-bar-number"><?='20%'?></span>
+									<div class="progress-bar-title"><?= $students[2]['skills'][0]?></div>
+									<span class="progress-bar-number"><?= $students[2]['percentages'][0]?></span>
 								</div>
 							</div>
 						</div>
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar" role="progressbar" style="width:70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Manning the Grill at a Cookout'?></div>
-									<span class="progress-bar-number"><?='70%'?></span>			  
+									<div class="progress-bar-title"><?= $students[2]['skills'][1]?></div>
+									<span class="progress-bar-number"><?= $students[2]['percentages'][1]?></span>			  
 								</div>
 							</div>
 						</div>
 						<div class="py-1">
 							<div class="progress">
 								<div class="progress-bar" role="progressbar" style="width:95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
-									<div class="progress-bar-title"><?='Cooking'?></div>
-									<span class="progress-bar-number"><?='95%'?></span>
+									<div class="progress-bar-title"><?= $students[2]['skills'][2]?></div>
+									<span class="progress-bar-number"><?= $students[2]['percentages'][2]?></span>
 								</div>
 							</div>
 						</div>
 						<h5 class="font-weight-normal">Fun fact</h5>
-						<p><?='I am not a very fun person'?></p>
+						<p><?= $students[2]['fact']?></p>
 						<a href="index.php"><?='Back To Index'?></a>
 					</div>
 				</div>
--- Assignment2/index.php
+++ Assignment2/index.php
@@ -1,5 +1,58 @@
 <?php
-	$students = array("Nick Windler" => 4, "John Doe" => 2, "Person One" => 3)
+$students = [
+	[
+		'name' => "Nick Windler",
+		'grade' => 4,
+		'job' => "Live Streaming",
+		'company' => "Twitch",
+		'email' => "windlern1@nku.edu",
+		'intro' => "While born in Indiana, I have spent the majority of my life living in LaGrange Kentucky.
+					I went to Oldham County Highschool and graduated in 2018. I made the decision to go to 
+					college here at NKU and now I am currently a senior CIT major getting ready to graduate 
+					this coming spring. I am currently working at the Cincinnati Insurance Company as an IT 
+					Resource Management Intern where I help develop apps on a small team. I expect I will 
+					continue doing some type of dev work in the future as well.",
+		'quote' => '"I can not think of a quote to put here" -Nick Windler',
+		'skills' => [
+			"Cooking" => "1%",
+			"Information Technologies" => "50%",
+			"Random Knowledge That Does Not Help Me in Life" => "90%"
+		],
+		'fact' => "I have two cats with three legs, I have been to the Grand Canyon, and I can eat a lot of 
+					Spaghetti in one sitting."
+	],
+	[
+		'name' => "John Doe",
+		'grade' => 2,
+		'job' => "Accountant",
+		'company' => "General Electric",
+		'email' => "doejohn@gmail.com",
+		'intro' => "I want to become an accountant to commit numerous amounts of white collar crime like Accounting Fraud.",
+		'quote' => '"You Either Die A Hero, Or You Live Long Enough To See Yourself Become The Villain" -Harvey Dent',
+		'skills' => [
+			"Being a Respectable Citizen" => "5%",
+			"Accounting" => "50%",
+			"Committing Crimes" => "99%"
+		],
+		'fact' => "I have never been to jail and I enjoy watching the news."
+	],
+	[
+		'name' => "Person One",
+		'grade' => 3,
+		'job' => "Chef",
+		'company' => "Create My Own",
+		'email' => "Personchef@gmail.com",
+		'intro' => "Hello, my name is person one. I was named Person One because the person who 
+					created me is not very creative so they could not think of anything else.",
+		'quote' => '"It really be like that sometimes" -anonymous',
+		'skills' => [
+			"Being a Fun Person" => "20%",
+			"Manning the Grill at a Cookout" => "70%",
+			"Cooking" => "95%"
+		],
+		'fact' => "I am not a very fun person"
+	]
+];
 ?>
 <!doctype html>
 <html lang="en">
@@ -28,60 +81,45 @@
 				</div>
 			</div>
 			<div class="row">
-				<?php foreach($students as $key => $val):?>
+				<?php for($i = 0; $i < count($students); $i++):?>
 					<!-- Single Advisor-->
 					<div class="col-12 col-sm-6 col-lg-3">
 						<div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
 							<!-- Team Thumb-->
-							<?php if($key == "Nick Windler"):?>
-								<div class="advisor_thumb"><a href="detail.php?name=<?=$key?>"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""></a>
-									<div class="social-info"><a href="detail.php?name=<?=$key?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-linkedin"></i></a></div><div class="social-info"><a href="detail.php?name=<?=$key?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-linkedin"></i></a></div>
+							<?php if($students[$i]['name'] == "Nick Windler"):?>
+								<div class="advisor_thumb"><a href="detail.php?name=<?=$students[$i]['name']?>"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""></a>
+									<div class="social-info"><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-linkedin"></i></a></div>
 								</div>
 							<?php endif;?>
-							<?php if($key == "John Doe"):?>
-								<div class="advisor_thumb"><a href="detail.php?name=<?=$key?>"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a>
-									<div class="social-info"><a href="detail.php?name=<?=$key?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-linkedin"></i></a></div>
+							<?php if($students[$i]['name'] == "John Doe"):?>
+								<div class="advisor_thumb"><a href="detail.php?name=<?=$students[$i]['name']?>"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a>
+									<div class="social-info"><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-linkedin"></i></a></div>
 								</div>
 							<?php endif;?>
-							<?php if($key == "Person One"):?>
-								<div class="advisor_thumb"><a href="detail.php?name=<?=$key?>"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""></a>
-									<div class="social-info"><a href="detail.php?name=<?=$key?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$key?>"><i class="fa fa-linkedin"></i></a></div>
+							<?php if($students[$i]['name'] == "Person One"):?>
+								<div class="advisor_thumb"><a href="detail.php?name=<?=$students[$i]['name']?>"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""></a>
+									<div class="social-info"><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-facebook"></i></a><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-twitter"></i></a><a href="detail.php?name=<?=$students[$i]['name']?>"><i class="fa fa-linkedin"></i></a></div>
 								</div>
 							<?php endif;?>
 							<!-- Team Details-->
 							<div class="single_advisor_details_info">
-								<h6><?="$key"?></h6>
-								<?php if($val == 4):?>
+								<h6><?=$students[$i]['name']?></h6>
+								<?php if($students[$i]['grade'] == 4):?>
 									<p class="designation"><?="Senior NKU Student"?></p>
 								<?php endif;?>
-								<?php if($val == 3):?>
+								<?php if($students[$i]['grade'] == 3):?>
 									<p class="designation"><?="Junior NKU Student"?></p>
 								<?php endif;?>
-								<?php if($val == 2):?>
+								<?php if($students[$i]['grade'] == 2):?>
 									<p class="designation"><?="Sophmore NKU Student"?></p>
 								<?php endif;?>
-								<?php if($val == 1):?>
+								<?php if($students[$i]['grade'] == 1):?>
 									<p class="designation"><?="Freshman NKU Student"?></p>
 								<?php endif;?>
 							</div>
 						</div>
 					</div>
-				<?php endforeach;?>
-				<!-- Single Advisor-->
-				<div class="col-12 col-sm-6 col-lg-3">
-					<div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
-						<!-- Team Thumb-->
-						<div class="advisor_thumb"><a href="detail.php"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""></a>
-							<!-- Social Info-->
-							<div class="social-info"><a href="detail.php"><i class="fa fa-facebook"></i></a><a href="detail.php"><i class="fa fa-twitter"></i></a><a href="detail.php"><i class="fa fa-linkedin"></i></a></div>
-						</div>
-						<!-- Team Details-->
-						<div class="single_advisor_details_info">
-							<h6>Niloy Islam</h6>
-							<p class="designation">Marketing Manager</p>
-						</div>
-					</div>
-				</div>
+				<?php endfor;?>
 			</div>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>

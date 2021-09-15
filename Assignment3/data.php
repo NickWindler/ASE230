cb5394e68af53debcc708e03894e355ce20651f8
NickWindler <89546808+NickWindler@users.noreply.github.com>
2021-09-15 16:28:20 -0400

    Add files via upload

--- Assignment3/data.php
+++ Assignment3/data.php
@@ -6,9 +6,17 @@ $students = [
 		'job' => "Live Streaming",
 		'company' => "Twitch",
 		'email' => "windlern1@nku.edu",
-		'skills' => [
-			"Cooking", "Information Technologies", "Random Knowledge That Does Not Help Me in Life"
-		]
+		'intro' => "While born in Indiana, I have spent the majority of my life living in LaGrange Kentucky.
+					I went to Oldham County Highschool and graduated in 2018. I made the decision to go to 
+					college here at NKU and now I am currently a senior CIT major getting ready to graduate 
+					this coming spring. I am currently working at the Cincinnati Insurance Company as an IT 
+					Resource Management Intern where I help develop apps on a small team. I expect I will 
+					continue doing some type of dev work in the future as well.",
+		'quote' => '"I can not think of a quote to put here" -Nick Windler',
+		'skills' => [ "Cooking", "Information Technologies", "Random Knowledge That Does Not Help Me in Life" ],
+		'percentages' => [ "1%", "50%", "90%" ],
+		'fact' => "I have two cats with three legs, I have been to the Grand Canyon, and I can eat a lot of 
+					Spaghetti in one sitting."
 	],
 	[
 		'name' => "John Doe",
@@ -16,9 +24,11 @@ $students = [
 		'job' => "Accountant",
 		'company' => "General Electric",
 		'email' => "doejohn@gmail.com",
-		'skills' => [
-			"Being a Respectable Citizen", "Accounting", "Committing Crimes"
-		]
+		'intro' => "I want to become an accountant to commit numerous amounts of white collar crime like Accounting Fraud.",
+		'quote' => '"You Either Die A Hero, Or You Live Long Enough To See Yourself Become The Villain" -Harvey Dent',
+		'skills' => [ "Being a Respectable Citizen", "Accounting", "Committing Crimes" ],
+		'percentages' => [ "5%", "50%", "99%" ],
+		'fact' => "I have never been to jail and I enjoy watching the news."
 	],
 	[
 		'name' => "Person One",
@@ -26,9 +36,12 @@ $students = [
 		'job' => "Chef",
 		'company' => "Create My Own",
 		'email' => "Personchef@gmail.com",
-		'skills' => [
-			"Being a Fun Person", "Manning the Grill at a Cookout", "Cooking"
-		]
+		'intro' => "Hello, my name is person one. I was named Person One because the person who 
+					created me is not very creative so they could not think of anything else.",
+		'quote' => '"It really be like that sometimes" -anonymous',
+		'skills' => [ "Being a Fun Person", "Manning the Grill at a Cookout", "Cooking" ],
+		'percentages' => [ "20%", "70%", "95%" ],
+		'fact' => "I am not a very fun person"
 	]
 ];
 

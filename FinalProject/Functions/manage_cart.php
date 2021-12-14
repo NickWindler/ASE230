dfbac44760bd85c016c184518220b17c4cdcc109
NickWindler <89546808+NickWindler@users.noreply.github.com>
2021-12-13 22:46:12 -0500

    Delete FinalProject directory

--- FinalProject/Functions/manage_cart.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if($_GET['function'] == 'add')
-    $_SESSION['cart'][] = [$_GET['name'], $_GET['price'], $_GET['mealID']];
-if($_GET['function'] == 'remove')
-    array_splice($_SESSION['cart'], $_GET['index'], 1);
-header('Location:../Views/Orders/order_meal_select.php?typeID='.$_GET['typeID'].'&rID='.$_GET['rID'].'&rName='.$_GET['rName']);
-?>
--- FinalProject/Views/Authentication/sign-out.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['email']);
-	unset($_SESSION['password']);
-	unset($_SESSION['role']);
-	$_SESSION['logged'] = false;
-	session_destroy();
-	header("Location:../index.php");
-?>
